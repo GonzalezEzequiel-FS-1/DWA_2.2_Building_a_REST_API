bd71c36bb3ef28d2f4b24fb33600e73f63a22adf
Zeke <egonzalezhidalgo@student.fullsail.edu>
2024-10-18 14:42:50 +0000

    modified:   vite/src/utils/utils.js

--- vite/src/utils/utils.js
+++ vite/src/utils/utils.js
@@ -1,6 +1,6 @@
 
 export const API_BASE =
     import.meta.env.MODE === "development"
-        ? "http://23.117.213.174:3000/api/v1/"
+        ? "http://localhost:3000/api/v1/"
         : import.meta.env.VITE_REACT_APP_BASE_URL;
 
